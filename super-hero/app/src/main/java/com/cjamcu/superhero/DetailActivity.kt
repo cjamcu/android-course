8bf27bdcd544aca9fbf87006b95ff4c62b4c8220
Carlos Acosta <cjamcu@gmail.com>
2023-12-25 20:12:14 -0500

    take photo in home screen and pass to detail

--- super-hero/app/src/main/java/com/cjamcu/superhero/DetailActivity.kt
+++ super-hero/app/src/main/java/com/cjamcu/superhero/DetailActivity.kt
@@ -7,6 +7,8 @@ import com.cjamcu.superhero.databinding.ActivityDetailBinding
 class DetailActivity : AppCompatActivity() {
     companion object {
         const val SUPER_HERO_KEY = "superHero"
+        const val BITMAP_KEY = "bitmap"
+
 
     }
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,7 @@ class DetailActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val bundle = intent.extras!!
+        binding.heroImage.setImageBitmap(bundle.getParcelable(BITMAP_KEY))
         binding.superHero = bundle.getParcelable<SuperHero>(SUPER_HERO_KEY)!!
 
     }
--- super-hero/app/src/main/java/com/cjamcu/superhero/MainActivity.kt
+++ super-hero/app/src/main/java/com/cjamcu/superhero/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.cjamcu.superhero
 
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
+import androidx.core.graphics.drawable.toBitmap
 import com.cjamcu.superhero.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var heroImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        heroImage = binding.heroImage
+        binding.heroImage.setOnClickListener {
+         openCamera()
+        }
 
         binding.saveButton.setOnClickListener {
             val superHeroName = binding.heroNameEdit.text.toString()
@@ -22,11 +33,29 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun openCamera() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, 100)
+
+    }
+
     private fun openDetailActivity( superHero: SuperHero) {
         val intent =  Intent(this, DetailActivity::class.java)
         intent.putExtra(DetailActivity.SUPER_HERO_KEY, superHero)
+        intent.putExtra(DetailActivity.BITMAP_KEY, heroImage.drawable.toBitmap())
 
         startActivity(intent)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 100 && resultCode == RESULT_OK) {
+            val extras = data?.extras
+            val heroBitmap = extras?.getParcelable<Bitmap>("data")
+            this.heroImage.setImageBitmap(heroBitmap)
+
+        }
     }
 }
 
