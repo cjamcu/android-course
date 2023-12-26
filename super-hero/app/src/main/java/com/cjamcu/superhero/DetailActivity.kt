90c5a9d881553b4be1b07dc95eb09a25878d72d4
Carlos Acosta <cjamcu@gmail.com>
2023-12-25 19:45:15 -0500

    pass data from home screen to detail screen

--- super-hero/app/src/main/java/com/cjamcu/superhero/DetailActivity.kt
+++ super-hero/app/src/main/java/com/cjamcu/superhero/DetailActivity.kt
@@ -2,10 +2,33 @@ package com.cjamcu.superhero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cjamcu.superhero.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+    companion object {
+        const val SUPER_HERO_NAME_KEY = "superHeroName"
+        const val ALTER_EGO_KEY = "alterEgo"
+        const val BIO_KEY = "bio"
+        const val POWER_KEY = "power"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bundle = intent.extras!!
+
+        val superHeroName = bundle.getString(SUPER_HERO_NAME_KEY)
+        val alterEgo = bundle.getString (ALTER_EGO_KEY)
+        val bio = bundle.getString (BIO_KEY)
+        val power = bundle.getFloat(POWER_KEY)
+
+        binding.heroName.text = superHeroName
+        binding.alterEgoText.text = alterEgo
+        binding.bioText.text = bio
+        binding.ratingBar.rating = power
+
+
+
     }
 }
--- super-hero/app/src/main/java/com/cjamcu/superhero/MainActivity.kt
+++ super-hero/app/src/main/java/com/cjamcu/superhero/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.cjamcu.superhero
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cjamcu.superhero.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.saveButton.setOnClickListener {
+            val superHeroName = binding.heroNameEdit.text.toString()
+            val alterEgo = binding.heroAlterEgoEdit.text.toString()
+            val bio = binding.heroBioEdit.text.toString()
+            val power = binding.ratingBar.rating
+
+
+
+            openDetailActivity(superHeroName, alterEgo, bio, power)
+        }
+    }
+
+    private fun openDetailActivity( superHeroName: String, alterEgo: String, bio: String, power: Float) {
+        val intent =  Intent(this, DetailActivity::class.java)
+        intent.putExtra(DetailActivity.SUPER_HERO_NAME_KEY, superHeroName)
+        intent.putExtra(DetailActivity.ALTER_EGO_KEY, alterEgo)
+        intent.putExtra(DetailActivity.BIO_KEY, bio)
+        intent.putExtra(DetailActivity.POWER_KEY, power)
+        startActivity(intent)
     }
 }
+
