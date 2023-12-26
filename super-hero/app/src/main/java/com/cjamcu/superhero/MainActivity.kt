15cc0319d2f7ad855465a7e08a8b1d2cf397fc18
Carlos Acosta <cjamcu@gmail.com>
2023-12-25 20:16:35 -0500

    migrate to new api of get photo from intent

--- super-hero/app/src/main/java/com/cjamcu/superhero/MainActivity.kt
+++ super-hero/app/src/main/java/com/cjamcu/superhero/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.cjamcu.superhero
 
 import android.content.Intent
-import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
 import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.graphics.drawable.toBitmap
 import com.cjamcu.superhero.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var heroImage: ImageView
+
+    private val getContent = registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap ->
+        heroImage.setImageBitmap(bitmap)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -34,9 +37,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun openCamera() {
-        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivityForResult(cameraIntent, 100)
-
+        getContent.launch(null)
     }
 
     private fun openDetailActivity( superHero: SuperHero) {
@@ -47,15 +48,5 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if(requestCode == 100 && resultCode == RESULT_OK) {
-            val extras = data?.extras
-            val heroBitmap = extras?.getParcelable<Bitmap>("data")
-            this.heroImage.setImageBitmap(heroBitmap)
-
-        }
-    }
 }
 
