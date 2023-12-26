a7200ad1f2fe9443b15ea81e55d729f8a2f145f8
Carlos Acosta <cjamcu@gmail.com>
2023-12-25 19:27:59 -0500

    add detail of super hero screen

---
+++ super-hero/app/src/main/java/com/cjamcu/superhero/DetailActivity.kt
@@ -0,0 +1,11 @@
+package com.cjamcu.superhero
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
