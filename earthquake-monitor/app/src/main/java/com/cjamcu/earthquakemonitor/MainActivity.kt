c5660a954961e3b3fed38c337d4579e395af8a1a
Carlos Acosta <cjamcu@gmail.com>
2023-12-25 23:41:42 -0500

    refactor to use viewModel

--- earthquake-monitor/app/src/main/java/com/cjamcu/earthquakemonitor/MainActivity.kt
+++ earthquake-monitor/app/src/main/java/com/cjamcu/earthquakemonitor/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.cjamcu.earthquakemonitor
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.cjamcu.earthquakemonitor.databinding.ActivityMainBinding
 
@@ -15,23 +16,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
-        val eqList = mutableListOf<Earthquake>()
-
+        val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
         val adapter = EqAdapter()
         binding.recyclerView.adapter = adapter
-        adapter.submitList(eqList)
+
+        viewModel.eqList.observe(this) {
+            adapter.submitList(it)
+            handleEmptyState(it)
+        }
 
         adapter.onItemClickListener = {
-           Toast.makeText(this, it.place, Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, it.place, Toast.LENGTH_SHORT).show()
         }
 
-        if(eqList.isEmpty()){
+    }
+
+    private fun handleEmptyState(it: MutableList<Earthquake>) {
+        if (it.isEmpty()) {
             binding.eqEmptyView.visibility = View.VISIBLE
-        }else{
+        } else {
             binding.eqEmptyView.visibility = View.GONE
         }
-
-
     }
 }
