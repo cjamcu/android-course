54ec8c0d8717fea0c4f5ca6e57fdc8e238dced32
Carlos Acosta <cjamcu@gmail.com>
2023-12-25 21:40:44 -0500

    use liveData and add two button and three button

--- basketball-score/app/src/main/java/com/cjamcu/basketballscore/MainActivity.kt
+++ basketball-score/app/src/main/java/com/cjamcu/basketballscore/MainActivity.kt
@@ -16,34 +16,60 @@ class MainActivity : AppCompatActivity() {
 
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
-        val localScoreText = binding.localScoreText
-        val visitorScoreText = binding.visitorScoreText
-        binding.localMinusButton.setOnClickListener {
-            viewModel.minusPointsToScore(1, true)
-            localScoreText.text = viewModel.localScore.toString()
-
+        viewModel.localScore.observe(this) {
+            binding.localScoreText.text = it.toString()
         }
 
-        binding.localPlusButton.setOnClickListener {
-            viewModel.addPointsToScore(1, true)
-            localScoreText.text = viewModel.localScore.toString()
+        viewModel.visitorScore.observe(this) {
+            binding.visitorScoreText.text = it.toString()
+        }
+
+
+        // Minus local
+        binding.localMinusButton.setOnClickListener {
+            viewModel.minusPointsToScore(1, true)
         }
 
         binding.visitorMinusButton.setOnClickListener {
             viewModel.minusPointsToScore(1, false)
-            visitorScoreText.text = viewModel.visitorScore.toString()
+        }
 
+        binding.localPlusButton.setOnClickListener {
+            viewModel.addPointsToScore(1, true)
+        }
+
+        binding.localPlusTwoButton.setOnClickListener {
+            viewModel.addPointsToScore(2, true)
+        }
+
+        binding.localPlusThreeButton.setOnClickListener {
+            viewModel.addPointsToScore(3, true)
+        }
+
+
+
+        binding.visitorMinusButton.setOnClickListener {
+            viewModel.minusPointsToScore(1, false)
         }
 
         binding.visitorPlusButton.setOnClickListener {
             viewModel.addPointsToScore(1, false)
-            visitorScoreText.text = viewModel.visitorScore.toString()
         }
+
+
+        binding.visitorPlusTwoButton.setOnClickListener {
+            viewModel.addPointsToScore(2, false)
+        }
+
+
+        binding.visitorPlusThreeButton.setOnClickListener {
+            viewModel.addPointsToScore(3, false)
+        }
+
+
 
         binding.restartButton.setOnClickListener {
             viewModel.resetScore()
-            localScoreText.text = viewModel.localScore.toString()
-            visitorScoreText.text = viewModel.visitorScore.toString()
         }
     }
 }
--- basketball-score/app/src/main/java/com/cjamcu/basketballscore/MainViewModel.kt
+++ basketball-score/app/src/main/java/com/cjamcu/basketballscore/MainViewModel.kt
@@ -1,33 +1,48 @@
 package com.cjamcu.basketballscore
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel(){
-    var localScore = 0
-    var visitorScore = 0
+class MainViewModel : ViewModel() {
+    var localScore: MutableLiveData<Int> = MutableLiveData()
+    var visitorScore: MutableLiveData<Int> = MutableLiveData()
 
-    fun resetScore(){
-        localScore = 0
-        visitorScore = 0
+    fun resetScore() {
+        localScore.value = 0
+        visitorScore.value = 0
     }
 
-    fun addPointsToScore(points: Int, isLocal: Boolean){
-        if (isLocal){
-            localScore += points
-        }else{
-            visitorScore += points
+    init {
+        resetScore()
+    }
+
+    fun addPointsToScore(points: Int, isLocal: Boolean) {
+        if (isLocal) {
+            localScore.value = localScore.value?.plus(points)
+        } else {
+            visitorScore.value = visitorScore.value?.plus(points)
         }
     }
 
-    fun minusPointsToScore(points: Int, isLocal: Boolean){
-        if (isLocal){
-            if (localScore > 0){
-                localScore -= points
-            }
-        }else{
-            if (visitorScore > 0){
-                visitorScore -= points
-            }
+    fun minusPointsToScore(points: Int, isLocal: Boolean) {
+
+        if (isLocal && localScore.value == 0) {
+            return
+        }
+
+        if (!isLocal && visitorScore.value == 0) {
+            return
+        }
+
+
+        if (isLocal) {
+
+            localScore.value = localScore.value!!.minus(points)
+
+        } else {
+
+            visitorScore.value = visitorScore.value!!.minus(points)
+
         }
     }
 
