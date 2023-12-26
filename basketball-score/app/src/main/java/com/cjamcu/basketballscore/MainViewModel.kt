d94867fc8b274c2a88025459852a3b416c6e4d48
Carlos Acosta <cjamcu@gmail.com>
2023-12-25 21:44:04 -0500

    make private liveDataMutable and expose LiveData

--- basketball-score/app/src/main/java/com/cjamcu/basketballscore/MainViewModel.kt
+++ basketball-score/app/src/main/java/com/cjamcu/basketballscore/MainViewModel.kt
@@ -1,15 +1,22 @@
 package com.cjamcu.basketballscore
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    var localScore: MutableLiveData<Int> = MutableLiveData()
-    var visitorScore: MutableLiveData<Int> = MutableLiveData()
+    private var _localScore: MutableLiveData<Int> = MutableLiveData()
+    private var _visitorScore: MutableLiveData<Int> = MutableLiveData()
+
+    val localScore: LiveData<Int>
+        get() = _localScore
+
+    val visitorScore: LiveData<Int>
+        get() = _visitorScore
 
     fun resetScore() {
-        localScore.value = 0
-        visitorScore.value = 0
+        _localScore.value = 0
+        _visitorScore.value = 0
     }
 
     init {
@@ -18,30 +25,30 @@ class MainViewModel : ViewModel() {
 
     fun addPointsToScore(points: Int, isLocal: Boolean) {
         if (isLocal) {
-            localScore.value = localScore.value?.plus(points)
+            _localScore.value = _localScore.value?.plus(points)
         } else {
-            visitorScore.value = visitorScore.value?.plus(points)
+            _visitorScore.value = _visitorScore.value?.plus(points)
         }
     }
 
     fun minusPointsToScore(points: Int, isLocal: Boolean) {
 
-        if (isLocal && localScore.value == 0) {
+        if (isLocal && _localScore.value == 0) {
             return
         }
 
-        if (!isLocal && visitorScore.value == 0) {
+        if (!isLocal && _visitorScore.value == 0) {
             return
         }
 
 
         if (isLocal) {
 
-            localScore.value = localScore.value!!.minus(points)
+            _localScore.value = _localScore.value!!.minus(points)
 
         } else {
 
-            visitorScore.value = visitorScore.value!!.minus(points)
+            _visitorScore.value = _visitorScore.value!!.minus(points)
 
         }
     }
